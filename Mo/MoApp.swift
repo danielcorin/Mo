64a8383574966437db7a5cffea6e5e6dae1459cf
Dan Corin <dcorin6@gmail.com>
2026-07-13 12:16:08 -0400

    Initial Commit

---
+++ Mo/MoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoApp.swift
+//  Mo
+//
+//  Created by Daniel Corin on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct MoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
